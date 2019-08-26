e345e4f8eb2d5d09d899ea96b1ec6361cf1a5d23
Nilton Mitsuharu Sugawara <nilton.sugawara@gmail.com>
2019-08-26 01:05:49 -0300

    relembrando conceitos básicos

---
+++ js/main.js
@@ -0,0 +1,69 @@
+
+/*
+var d = new Date();
+var diaSem = d.getDay();
+if(diaSem = 1){
+  alert(getDay());
+}
+
+
+
+/*
+
+var count;
+for (count = 1; count < 5; count++) {
+  alert(count);
+}
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+
+/*
+
+var idade = prompt("qual sua idade")
+if(idade >= 18){
+  alert("maior de idade");
+}else {
+  alert("menor de idade");
+};
+
+*/
+/*
+
+var frutas = [{nome:"limão", cor:"verde"},{nome:"uva", cor:"roxa"}];
+var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta);
+console.log(fruta.nome);
+console.log(frutas.nome);
+
+*/
+//alert(fruta.cor);
+/*
+var lista = ["maça","pêra","laranja"];
+console.log(lista[1]);
+lista.push("uva")
+console.log(lista);
+
+console.log(lista[2].toString()[4]);
+console.log(lista.join("|"));
+
+*/
+/*var nome = "Nilton Mitsuharu";
+var idade = 29;
+var idade2=  10;
+var frase = "teste frase"
+//alert(nome+" tem "+idade);
+//alert(idade+idade);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase);
+console.log(frase.replace("teste","aplicando a"));
+console.log(frase.toLowerCase());
+*/
